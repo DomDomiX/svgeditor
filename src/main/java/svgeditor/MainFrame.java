6674cfdb1bf2614acb8f6789d3160ea544d3055c
Dom <domdomix74@gmail.com>
2024-04-15 15:09:04 +0200

    Phase 2

--- src/main/java/svgeditor/MainFrame.java
+++ src/main/java/svgeditor/MainFrame.java
@@ -1,25 +1,31 @@
 package svgeditor;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class MainFrame extends JFrame
 {
+    private final JTabbedPane tabsPane;
     public MainFrame()
     {
-        JFrame frame = new JFrame("SVG Editor");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setExtendedState(MAXIMIZED_BOTH);
+        super("SVG Editor");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(MAXIMIZED_BOTH);
+
+        tabsPane = new JTabbedPane();
+        add(tabsPane);
 
         MujPanel panel = new MujPanel();
+        MujPanel panelSVG = new MujPanel();
+
+        tabsPane.addTab("Editor", panel);
+        tabsPane.addTab("SVG", panelSVG);
+
         panel.addTvar(new Obdelnik(300,200,"#2C2F93",80, 60));
         panel.addTvar(new Kruh(400, 500, 40, "#2C2F93"));
         panel.addTvar(new Line(600, 700, "#2C2F93", 50, 50));
         panel.addTvar(new Oval(800, 300, "#2C2F93", 100, 50));
 
-        frame.add(panel);
-
-        frame.setVisible(true);
+        setVisible(true);
     }
 
 }
